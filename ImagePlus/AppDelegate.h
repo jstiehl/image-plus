385e2cd7494e39eff2cdd0a1934e1164a314cefc
James Stiehl <jstiehl@gs-mbp15-james.local>
2015-01-08 18:39:00 -0800

    Initial Commit

---
+++ ImagePlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePlus
+//
+//  Created by James Stiehl on 1/8/15.
+//  Copyright (c) 2015 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePlus
+//
+//  Created by James Stiehl on 1/8/15.
+//  Copyright (c) 2015 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
